87da255886fb8e0428b3ea23218b2705c4b69015
LevanSamadashvili <lsama19@freeuni.edu.ge>
2021-08-10 15:42:58 +0400

    Added hash function for password

--- Auction/src/main/java/Models/Password.java
+++ Auction/src/main/java/Models/Password.java
@@ -1,5 +1,8 @@
 package Models;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
 public class Password {
@@ -21,9 +24,42 @@ public class Password {
         return Objects.equals(password, password1.password);
     }
 
-    @Override
-    public int hashCode() {
-        // TODO: implement better hash function later
-        return Objects.hash(password);
+    /**
+     * Given a byte[] array, produces a hex String,
+     * such as "234a6f". with 2 chars for each byte in the array.
+     * @param bytes bytes to convert to string
+     * @return printableHash
+     */
+    private String hexToString(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (int i=0; i<bytes.length; i++) {
+            int val = bytes[i];
+            val = val & 0xff;  // remove higher bits, sign
+            if (val<16) builder.append('0'); // leading 0
+            builder.append(Integer.toString(val, 16));
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Hashes password string using SHA-256 algorithm
+     * @return hash string of password
+     */
+    public String hash() {
+        MessageDigest md = null;
+        byte[] byteArray = password.getBytes(StandardCharsets.UTF_8);
+
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            md.update(byteArray);
+            byte[] hashBytes = md.digest();
+
+            String printableHash = hexToString(hashBytes);
+            return printableHash;
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+
+        return "";
     }
 }
