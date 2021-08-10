2c62d5233c0dc1176cc9010856186bbe511c862d
LevanSamadashvili <lsama19@freeuni.edu.ge>
2021-08-10 12:42:02 +0400

    Added Status to user class

--- Auction/src/main/java/Models/User.java
+++ Auction/src/main/java/Models/User.java
@@ -3,6 +3,15 @@ package Models;
 import java.util.Objects;
 
 public class User {
+    public static final int SILVER = 0;
+    public static final int GOLD = 1;
+    public static final int PLATINUM = 2;
+    public static final int STATUS_UNDEFINED = -1;
+
+    private static final int AUCTIONS_NEEDED_FOR_SILVER = 0;
+    private static final int AUCTIONS_NEEDED_FOR_GOLD = 10;
+    private static final int AUCTIONS_NEEDED_FOR_PLATINUM = 50;
+
     private int id;
     private int userInfoId;
     private String username;
@@ -12,6 +21,7 @@ public class User {
     private int numAuctionsWon;
     private int rating;
     private int numReviews;
+    private int status; // SILVER, GOLD, PLATINUM
 
     public User(int id, int userInfoId, String username, Password password,
                 boolean isDealer, boolean isAdmin, int numAuctionsWon,
@@ -25,6 +35,7 @@ public class User {
         this.numAuctionsWon = numAuctionsWon;
         this.rating = rating;
         this.numReviews = numReviews;
+        this.status = calculateStatus(numAuctionsWon);
     }
 
     public int getId() {
@@ -63,6 +74,10 @@ public class User {
         return numReviews;
     }
 
+    public int getStatus() {
+        return status;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -89,6 +104,7 @@ public class User {
 
     public void setNumAuctionsWon(int numAuctionsWon) {
         this.numAuctionsWon = numAuctionsWon;
+        this.status = calculateStatus(numAuctionsWon);
     }
 
     public void setRating(int rating) {
@@ -99,12 +115,35 @@ public class User {
         this.numReviews = numReviews;
     }
 
+    // Increments numAuctionsWon by given value "increment"
+    public void incrementNumAuctionsWon(int increment) {
+        this.numAuctionsWon += increment;
+        this.status = calculateStatus(this.numAuctionsWon);
+    }
+
+    // Increments numAuctionsWon by 1
+    public void incrementNumAuctionsWon() {
+        incrementNumAuctionsWon(1);
+    }
+
+    // Calculates user's status using number of auctions won by user
+    private int calculateStatus(int numAuctionsWon) {
+        if (numAuctionsWon >= User.AUCTIONS_NEEDED_FOR_PLATINUM) {
+            return User.PLATINUM;
+        } else if (numAuctionsWon >= User.AUCTIONS_NEEDED_FOR_GOLD) {
+            return User.GOLD;
+        } else if (numAuctionsWon >= User.AUCTIONS_NEEDED_FOR_SILVER) {
+            return User.SILVER;
+        }
+
+        return User.STATUS_UNDEFINED;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id == user.id && userInfoId == user.userInfoId && isDealer == user.isDealer && isAdmin == user.isAdmin && numAuctionsWon == user.numAuctionsWon && rating == user.rating && numReviews == user.numReviews && username.equals(user.username) && password.equals(user.password);
+        return id == user.id && userInfoId == user.userInfoId && isDealer == user.isDealer && isAdmin == user.isAdmin && numAuctionsWon == user.numAuctionsWon && rating == user.rating && numReviews == user.numReviews && status == user.status && Objects.equals(username, user.username) && Objects.equals(password, user.password);
     }
-
 }
