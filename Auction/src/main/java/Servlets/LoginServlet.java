4b4354a1d062347809d563612be0b4f00bc12112
LevanSamadashvili <lsama19@freeuni.edu.ge>
2021-08-11 11:35:52 +0400

    Finished Login

--- Auction/src/main/java/Servlets/LoginServlet.java
+++ Auction/src/main/java/Servlets/LoginServlet.java
@@ -1,14 +1,19 @@
 package Servlets;
 
+import DAO.UserDAO;
+import Helper.GeneralConstants;
 import Helper.Hasher;
+import Models.User;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.SchemaOutputResolver;
 import java.io.IOException;
 
-public class LoginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet implements GeneralConstants {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
@@ -16,6 +21,20 @@ public class LoginServlet extends HttpServlet {
 
         String passwordHash = Hasher.hash(password);
 
-        System.out.println("Username: " + username + " Password: " + passwordHash);
+        ServletContext servletContext = getServletContext();
+        UserDAO userDAO = (UserDAO)servletContext.getAttribute(USER_DAO);
+        User foundUser = userDAO.getUser(username);
+
+        if (foundUser == null) {
+            // TODO: Notify user on the webpage
+            System.out.println("User doesn't exist");
+            request.getRequestDispatcher("Pages/home.jsp").forward(request, response);
+        } else if (!passwordHash.equals(foundUser.getPassword())) {
+            // TODO: Notify user on the webpage
+            System.out.println("Invalid password");
+            request.getRequestDispatcher("Pages/home.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("Pages/successful-login.jsp").forward(request, response);
+        }
     }
 }
